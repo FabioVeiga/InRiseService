3b6775e757a6d378339763ee32ac130f991e3412
Fabio Veiga <fabinholveiga@gmail.com>
2024-04-10 06:28:31 -0300

    ajuste swagger

--- InRiseService.Presentation/Program.cs
+++ InRiseService.Presentation/Program.cs
@@ -76,11 +76,8 @@ builder.Services.AddDbContext<ApplicationContext>(opt =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
