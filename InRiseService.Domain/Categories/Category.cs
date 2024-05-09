4ba016c9825ee7f32cb58cfebfc269569f65cc7e
Fabio Veiga <fabinholveiga@gmail.com>
2024-05-08 22:00:48 -0300

    add limite no nome da categoria

--- InRiseService.Domain/Categories/Category.cs
+++ InRiseService.Domain/Categories/Category.cs
@@ -1,7 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace InRiseService.Domain.Categories
 {
     public class Category : BaseDomain
     {
+        [Display(Name = "Nome")]
+        [Required(ErrorMessage = "{0} é obrigatório!")]
+        [MinLength(1, ErrorMessage = "{0} deve conter no mínimo {1} caracteres!")]
+        [MaxLength(100, ErrorMessage = "{0} deve conter no máximo {1} caracteres!")]
         public string Name { get; set; } = null!;
     }
 }
