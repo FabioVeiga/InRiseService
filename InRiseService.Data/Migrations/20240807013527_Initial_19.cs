f93f86d98c8784e7b4467c5559acb93a00a34e13
Yara Regina <yara.regina@hotmail.com>
2024-08-06 22:38:17 -0300

    aplicando migration

---
+++ InRiseService.Data/Migrations/20240807013527_Initial_19.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InRiseService.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial_19 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CategoryId",
+                table: "ImagensProducts",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ImagensProducts_CategoryId",
+                table: "ImagensProducts",
+                column: "CategoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ImagensProducts_Categories_CategoryId",
+                table: "ImagensProducts",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ImagensProducts_Categories_CategoryId",
+                table: "ImagensProducts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ImagensProducts_CategoryId",
+                table: "ImagensProducts");
+
+            migrationBuilder.DropColumn(
+                name: "CategoryId",
+                table: "ImagensProducts");
+        }
+    }
+}
