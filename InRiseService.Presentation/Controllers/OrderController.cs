f9d60ae0cfc960cf89c318ae609dc8e9f2958e65
Fabio Veiga <fabinholveiga@gmail.com>
2025-02-03 20:36:17 -0300

    ajustando permissao

--- InRiseService.Presentation/Controllers/OrderController.cs
+++ InRiseService.Presentation/Controllers/OrderController.cs
@@ -93,7 +93,7 @@ namespace InRiseService.Presentation.Controllers
 
         [HttpGet]
         [Route("{id}")]
-        [Authorize(Roles = "Admin")]
+        [Authorize(Roles = "Admin, User")]
         public async Task<IActionResult> GetById(int id)
         {
             try
@@ -119,7 +119,7 @@ namespace InRiseService.Presentation.Controllers
 
         [HttpGet]
         [Route("user/{userId}")]
-        [Authorize(Roles = "Admin")]
+        [Authorize(Roles = "Admin, User")]
         public async Task<IActionResult> GetByUserId(int userId)
         {
             try
