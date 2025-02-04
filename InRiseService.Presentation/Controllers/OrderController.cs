4f5bd93c58d6abee62e13d1d42d03d6be3f143f6
Fabio Veiga <fabinholveiga@gmail.com>
2025-02-04 11:43:03 -0300

    historic-by-orderid ajustando 404

--- InRiseService.Presentation/Controllers/OrderController.cs
+++ InRiseService.Presentation/Controllers/OrderController.cs
@@ -201,7 +201,7 @@ namespace InRiseService.Presentation.Controllers
             try
             {
                 var result = await _orderService.GetOrderHistoricByOrderId(orderId);
-                if (result == null) return NotFound();
+                if (result.Count() == 0) return NotFound();
                 var response = new ApiResponse<dynamic>(
                     StatusCodes.Status200OK,
                     result
